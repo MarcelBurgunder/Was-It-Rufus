58ea6b8841b685b5f19af9beb731e6ceefa57591
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-01-23 15:48:45 -0600

    Update wasItRufus.py

--- wasItRufus.py
+++ wasItRufus.py
@@ -3,6 +3,8 @@ from git import Repo, exc
 from os import path
 from time import time
 
+sevenDays = 604800 # denotes the duration of seven days in seconds
+
 def main():
     """
     Main routine for the blame rufus program.
@@ -13,7 +15,6 @@ def main():
         3) Whether the current head commit was authored in the last week
         4) Whether the current head commit was authored by Rufus
     """
-    sys.argv = ["dir", "dir"]
     dirPath = sys.argv[0] if len(sys.argv) > 1 else "./"
     if not path.exists(dirPath):
         print("The provided path: \"" + str(dirPath) + "\" does not exist")
@@ -50,7 +51,7 @@ def hadRecentCommit(repo: Repo) -> bool:
     Returns True if the current head commit was authored last week, False otherwise.
     This specifically verifies whether the head was committed within 604800 seconds (7 full days) from the current day and time.
     """
-    return time()-repo.head.commit.committed_date < 604800
+    return time()-repo.head.commit.committed_date < sevenDays
 
 def blameRufus(repo: Repo) -> bool:
     """
